c68fc6b36080ff6c4b5af22cf2f070bdbe3aa654
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-03-02 10:39:01 -0300

    exibição e eventUpdate

--- src/app/cadastro-anuncio/cadastro-anuncio.component.ts
+++ src/app/cadastro-anuncio/cadastro-anuncio.component.ts
@@ -8,10 +8,40 @@ import 'sweetalert2/src/sweetalert2.scss'
   styleUrls: ['./cadastro-anuncio.component.scss']
 })
 export class CadastroAnuncioComponent implements OnInit {
+  tipo: string;
+  quota: string;
 
-  constructor() { }
+
+  constructor() {
+    this.tipo = "Tipo"
+    this.quota = "Quota"
+  }
 
   ngOnInit(): void {
+  }
+
+  setTipoImovel(event: string){
+    if(event === 'casa'){
+      this.tipo = "Casa"
+    }
+    if(event === 'apartamento'){
+      this.tipo = "Apartamento"
+    }
+    if(event === 'terreno'){
+      this.tipo = "Terreno"
+    }
+    if(event === 'ponto'){
+      this.tipo = "Ponto Comercial"
+    }
+  }
+
+  setQuota(event: string){
+    if(event === 'vista'){
+      this.quota = "À vista"
+    }
+    if(event === 'parcelado'){
+      this.quota = "Parcelado"
+    }
   }
 
   ativacaoSelo(){
@@ -19,6 +49,10 @@ export class CadastroAnuncioComponent implements OnInit {
   }
 
   ativacaoAnuncio(){
+
+  }
+
+  eventUpdate(){
 
   }
 }
