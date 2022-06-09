6256c1dd5d20c8bb56bbfa480dc2c05a5ff1a838
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-06-08 23:24:59 -0300

    Refatoração de código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroCompradorComponent } from './cadastro-comprador/cadastro-comprador.component';
-import { CadastroVendedorComponent } from './cadastro-vendedor/cadastro-vendedor.component';
 import { FeedCompradorAnunciosComponent } from './feed-comprador-anuncios/feed-comprador-anuncios.component';
-import { FeedVendedorAnunciosComponent } from './feed-vendedor-anuncios/feed-vendedor-anuncios.component';
 import { routing } from './app.routing';
 import { EsqueciComponent } from './esqueci/esqueci.component';
 import { MeuPerfilComponent } from './meu-perfil/meu-perfil.component';
 import { CadastroAnuncioComponent } from './cadastro-anuncio/cadastro-anuncio.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CadastroCompradorComponent,
-    CadastroVendedorComponent,
     FeedCompradorAnunciosComponent,
-    FeedVendedorAnunciosComponent,
     EsqueciComponent,
     MeuPerfilComponent,
-    CadastroAnuncioComponent
+    CadastroAnuncioComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    routing,
-  ],
+  imports: [BrowserModule, AppRoutingModule, routing, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,24 +2,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 import { CadastroCompradorComponent } from './cadastro-comprador/cadastro-comprador.component';
-import { CadastroVendedorComponent } from './cadastro-vendedor/cadastro-vendedor.component';
 import { FeedCompradorAnunciosComponent } from './feed-comprador-anuncios/feed-comprador-anuncios.component';
-import { FeedVendedorAnunciosComponent } from './feed-vendedor-anuncios/feed-vendedor-anuncios.component';
 import { LoginComponent } from './login/login.component';
 import { EsqueciComponent } from './esqueci/esqueci.component';
 import { MeuPerfilComponent } from './meu-perfil/meu-perfil.component';
 import { CadastroAnuncioComponent } from './cadastro-anuncio/cadastro-anuncio.component';
 
-
 const app_routes: Routes = [
-    { path: 'cadastro-comprador', component: CadastroCompradorComponent},
-    { path: 'cadastro-vendedor', component: CadastroVendedorComponent},
-    { path: 'feed-comp-ads', component: FeedCompradorAnunciosComponent},
-    { path: 'feed-vend-cads', component: FeedVendedorAnunciosComponent},
-    { path: 'esqueci', component: EsqueciComponent},
-    { path: 'meu-perfil', component: MeuPerfilComponent},
-    { path: 'cadastro-anuncio', component: CadastroAnuncioComponent},
-    { path: '', component: LoginComponent}
+  { path: 'cadastro-comprador', component: CadastroCompradorComponent },
+  { path: 'feed-comp-ads', component: FeedCompradorAnunciosComponent },
+  { path: 'esqueci', component: EsqueciComponent },
+  { path: 'meu-perfil', component: MeuPerfilComponent },
+  { path: 'cadastro-anuncio', component: CadastroAnuncioComponent },
+  { path: '', component: LoginComponent },
 ];
 
-export const routing: ModuleWithProviders<any> = RouterModule.forRoot(app_routes);
+export const routing: ModuleWithProviders<any> =
+  RouterModule.forRoot(app_routes);
